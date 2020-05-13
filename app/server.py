7c8d4a82a02007de8d91b7b94c159389ba620c56
smegyery <64656408+smegyery@users.noreply.github.com>
2020-05-12 21:01:43 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1WTB6CjYQdyJHO9wFUip_2oYjCBUZgTlm'
+export_file_url = 'https://www.dropbox.com/s/brt1r4hhvy68dyj/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['1_hand', '2_hand']
